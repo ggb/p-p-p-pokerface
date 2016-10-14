38b49371f8c74469ca8a967d374e6769cdf9a897
ggb <gregorbg@web.de>
2016-10-14 17:18:21 +0200

    first half

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,25 +1,32 @@
 (ns p-p-p-pokerface)
 
-(defn rank [card]
-  nil)
+(defn suit [[_ snd]]
+  (str snd))
 
-(defn suit [card]
-  nil)
+(defn rank [[fst _]]
+  (let [special-rank {\T 10, \J 11, \Q 12, \K 13 \A 14}]
+    (if (contains? special-rank fst)
+      (get special-rank fst)
+      (Integer/valueOf (str fst)))))
+
+(defn n-of-a-kind [hand n]
+  (= n (apply max (vals (frequencies (map rank hand))))))
 
 (defn pair? [hand]
-  nil)
+  (n-of-a-kind hand 2))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (n-of-a-kind hand 3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (n-of-a-kind hand 4))
 
 (defn flush? [hand]
-  nil)
+  (apply = (map suit hand)))
 
 (defn full-house? [hand]
-  nil)
+  (let [min-max (vals (frequencies [4 7 7 4 7]))]
+    (and (= 2 (apply min min-max)) (= 3 (apply max min-max)))))
 
 (defn two-pairs? [hand]
   nil)
